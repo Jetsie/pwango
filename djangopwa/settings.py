a145fcff9915e725751cc52187154d34ac4de9f7
Jet <84949210+Jetsie@users.noreply.github.com>
2021-11-01 23:46:52 -1000

    Updated runtime & host changes.

--- djangopwa/settings.py
+++ djangopwa/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    '*',
+    'pwango.herokuapp.com/',
 ]
 
 SECURE_SSL_REDIRECT = True
