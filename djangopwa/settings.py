5609430e232d44ef62359f7507cab138cd564b1a
Jet <84949210+Jetsie@users.noreply.github.com>
2021-11-03 10:01:22 -1000

    Added some more specified settings.

--- djangopwa/settings.py
+++ djangopwa/settings.py
@@ -137,7 +137,12 @@ PWA_APP_DESCRIPTION = 'A proxy box PWA.'
 PWA_APP_THEME_COLOR = '#8E05C2'
 PWA_APP_BACKGROUND_COLOR = '#000000'
 PWA_APP_DISPLAY = 'standalone'
-PWA_APP_ORIENTATION = 'portrait'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
 PWA_APP_ICONS = [{
         "src": "/static/images/icons/windows11/SmallTile.scale-100.png",
         "sizes": "71x71"
