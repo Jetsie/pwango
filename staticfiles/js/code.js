d0a1f09d01714ee7ad6bed9ed205e5b467e88f29
Jet <84949210+Jetsie@users.noreply.github.com>
2021-11-08 10:45:41 -1000

    Update code.js

--- staticfiles/js/code.js
+++ staticfiles/js/code.js
@@ -1,5 +1,5 @@
 textLabel("label","Hello World");
-
+playSpeech("Hello World!", "female", "English");
 timedLoop(1000, function() {
   setPosition("label", randomNumber(0,320), randomNumber(0,450));
 });
